2d7b56c2f6519b1a915247feddbdbd50f971a8ac
Papun1111 <gohanmohapatra@gmail.com>
2025-02-13 17:03:03 +0530

    updation in appcontext

--- frontend/src/Context/AppContext.jsx
+++ frontend/src/Context/AppContext.jsx
@@ -1,5 +1,5 @@
-import { createContext } from "react";
-
+import { createContext, useEffect, useState } from "react";
+import axios from "axios"
 
 const AppContext = createContext();
 
@@ -7,8 +7,19 @@ const AppProvider = ({ children }) => {
 
 const backendUrl="http://localhost:3000/api/"
  
-  const value = {backendUrl};
-
+  
+const [predictons,setPredictions]=useState([]);
+const fetchPredictonData=async () => {
+  const response=await axios.get(backendUrl+"/predictions");
+  setPredictions(...predictons,response.data);
+}
+useEffect(()=>{
+fetchPredictonData();
+},[])
+useEffect(()=>{
+  fetchPredictonData();
+  },[predictons])
+const value = {backendUrl,predictons};
   return (
     <AppContext.Provider value={value}>
       {children}
